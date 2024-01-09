2024b5c0774e0843216d386e6333c1ccf26e5a9c
C1PH3R <haseebaptech39@gmail.com>
2024-01-09 20:51:00 +0500

    PJS 1/9/24

---
+++ second.js
@@ -0,0 +1,48 @@
+let myData = {
+    name : "Peter",
+    city : "Karachi",
+    age : 54,
+    salary : 1000,
+    Garde : "C",
+};
+
+// Two Methods of getting data from an object
+// First Method 
+console.log(myData["city"]);
+console.log(myData["age"]);
+
+// Second Method
+console.log(myData.salary);
+console.log(myData.Garde);
+console.log(myData);
+
+// Getting Data with using Loop
+
+for(let i in myData){
+    console.log(i , myData[i]);
+}
+
+
+// Logical Operators
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log("End Operator");
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// Comparision
+// > Greater than
+// < less Than
+// >= Greater than equal to
+// <= less than equal to
+
+// == it means equal
+// === equal with type Check
+
+
