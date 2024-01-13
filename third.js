d1fa7684f9f12e5c31116167db97dc6437860cd4
C1PH3R <haseebaptech39@gmail.com>
2024-01-13 20:45:15 +0500

    save 1/13/2024

--- third.js
+++ third.js
@@ -1,27 +1,63 @@
-let age = 20
+// let age = 20
 
-if(age>=20){
-    console.log("You can Drive")
-}if(age<=20){
-    console.log("You can't Drive");
-}
+// if(age>=20){
+//     console.log("You can Drive")
+// }if(age<=20){
+//     console.log("You can't Drive");
+// }
 
-if(age>=20){
-    console.log("You can cast your Vote");
-}else{
-    console.log("You can't cast your Vote");
-}
+// if(age>=20){
+//     console.log("You can cast your Vote");
+// }else{
+//     console.log("You can't cast your Vote");
+// }
 
-let mode = prompt(`Enter Your Mode
-1)Dark
-2)Light`);
+// let mode = prompt(`Enter Your Mode
+// 1)Dark
+// 2)Light`);
 
-let color;
+// let color;
 
-if(mode ==="Dark"){
-    color="Black"
-}if(mode ==="Light"){
-    color="light"
-}
-
+// if(mode ==="Dark"){
+//     color="Black"
+// }if(mode ==="Light"){
+//     color="light"
+// }
 console.log("You Choose the Color ", color);
+
+
+for(let i=1; i<=10; i++){
+    console.log(`${2} x ${i} = ${2*i}`);
+}
+
+let myNum = 80;
+while(myNum<=84){
+    console.log("This is While Loop" , myNum)
+    myNum++;
+}
+let z = 44;
+do{
+    console.log("This is do while loop" , z);
+    z++;
+}while (z<=50);
+
+let gameNum = 95;
+let userNum = prompt("Enter Your Number");
+while(userNum != gameNum){
+    userNum = prompt("You Guess the Wrong Number : Guess Again");
+}
+swal("You Win")
+let obj = {
+    name : "Haseeb",
+    age : 16,
+    grade : "A+",
+    city : "KCI",
+    number : 123
+}
+
+for(let n in obj){
+    console.log(n,obj[n])
+}
+
+
+let user = prompt("Enter Your Number")
