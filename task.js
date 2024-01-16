6124f9475d842e1e90464a64c3f35b37fb038fa8
C1PH3R <haseebaptech39@gmail.com>
2024-01-16 20:54:42 +0500

    save 1/16/24

---
+++ task.js
@@ -0,0 +1,13 @@
+let usrname = prompt("Enter your Name");
+
+let math = prompt("Enter your Maths Marks");
+
+let urdu = prompt("Enter your Urdu Marks");
+
+let english = prompt("Enter your English Marks");
+
+let Phycics = prompt("Enter your Physics Marks");
+
+let Computer = prompt("Enter your Computer Marks");
+
+ 
