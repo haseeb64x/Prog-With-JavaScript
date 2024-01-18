9061f8b37be30634a88e58de4370e9a0de080b0e
C1PH3R <haseebaptech39@gmail.com>
2024-01-18 20:15:40 +0500

    save 1/18/24

--- task.js
+++ task.js
@@ -1,13 +1,31 @@
 let usrname = prompt("Enter your Name");
 
-let math = prompt("Enter your Maths Marks");
+let math = parseInt(prompt("Enter your Maths Marks"));
 
-let urdu = prompt("Enter your Urdu Marks");
+let urdu = parseInt(prompt("Enter your Urdu Marks"));
 
-let english = prompt("Enter your English Marks");
+let english = parseInt(prompt("Enter your English Marks"));
 
-let Phycics = prompt("Enter your Physics Marks");
+let Phycics = parseInt(prompt("Enter your Physics Marks"));
 
-let Computer = prompt("Enter your Computer Marks");
+let Computer = parseInt(prompt("Enter your Computer Marks"));
 
- 
+let obtainedMarks = math+urdu+english+Phycics+Computer;
+
+let per = obtainedMarks/500*100;
+
+console.log(per);
+
+if(per>=90 && per>=100){
+    console.log("Your Grade is A+")
+}else if(per>= 80 && per>= 90){
+    console.log("Your Grade is B")
+}else if(per>=60 && per<=70){
+    console.log("Your Grade is C");
+}
+else if(per>=50 && per<=40){
+    console.log("Your grade is D")
+}
+else{
+    console.log("you are fail")
+}
