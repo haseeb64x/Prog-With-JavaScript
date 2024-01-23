fb6e9e7525e72a5267184dd10bd5a081865d97b0
C1PH3R <haseebaptech39@gmail.com>
2024-01-23 20:13:12 +0500

    save 1/23/24

--- dom.js
+++ dom.js
@@ -6,3 +6,20 @@ obj.style.background = "#F5F5DC";
 obj.style.textAlign = "Center";
 obj.style.paddingTop = '10px';
 obj.style.paddingBottom = '10px';
+
+let btn = document.querySelector(".btn");
+let container = document.querySelector(".container");
+
+var flag = 0;
+btn.addEventListener("click", function(){
+    if(flag == 0) {
+        container.classList.add("s1");
+        container.classList.remove("s2");
+        flag = 1
+    }
+    else {
+        container.classList.add("s2");
+        container.classList.remove("s1");
+        flag = 0;
+    }
+})
