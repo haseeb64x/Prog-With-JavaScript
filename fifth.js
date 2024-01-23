2c87702a927e8f7d4808a7be1ca46105fa9d4293
C1PH3R <haseebaptech39@gmail.com>
2024-01-23 19:35:02 +0500

    save 1/23/24

--- fifth.js
+++ fifth.js
@@ -4,8 +4,14 @@
 // document.write(result);
 
 
-let grade = prompt("Enter your Grade");
-let per = parseInt(prompt("Enter your Percentage"));
+// let grade = prompt("Enter your Grade");
+// let per = parseInt(prompt("Enter your Percentage"));
 
-let result = per===90 && grade == "A" ||per===80 &&grade == "B"? "Pass" : "Fail";
+// let result = per===90 && grade == "A" ||per===80 &&grade == "B"? "Pass" : "Fail";
+// document.write(result);
+
+let grade = prompt("Enter Your Grade");
+let per = parseInt(prompt("Enter Your Percentage"));
+
+let result = grade ==="A" && per ===90 ||  grade ==="b" && per ===80 ? "Pass" : "Fail";
 document.write(result);
