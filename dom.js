5edaa51c2ddfa261a97f2e0d18eb4fe20ce9907c
C1PH3R <haseebaptech39@gmail.com>
2024-01-23 19:56:33 +0500

    save 1/23/24

---
+++ dom.js
@@ -0,0 +1,8 @@
+let obj = document.getElementById('first');
+console.log(obj);
+
+obj.innerHTML="Document Object Model";
+obj.style.background = "#F5F5DC";
+obj.style.textAlign = "Center";
+obj.style.paddingTop = '10px';
+obj.style.paddingBottom = '10px';
