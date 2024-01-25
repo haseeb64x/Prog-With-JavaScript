4d1068d37702b27756db87d1f3b417a855f7cc8e
C1PH3R <haseebaptech39@gmail.com>
2024-01-25 20:28:28 +0500

    save 1/25/2024

--- task2.js
+++ task2.js
@@ -6,4 +6,14 @@ dd[0].appendChild(ifld);
 ifld.addEventListener('keyup' , myf);
     function myf(){
         console.log("Typing...")
-    }
+        
+    };
+    let newbtn = document.createElement('button');
+        dd[0].appendChild(newbtn);
+        newbtn.innerHTML = "Clear";
+    newbtn.addEventListener("click", tb);
+        function tb(){
+            ifld.value = "";
+        }
+        
+        
